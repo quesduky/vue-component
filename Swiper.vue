380a089e8cb066a359a9b52edab16ea81672364b
ling <35324247+quesduky@users.noreply.github.com>
2020-07-17 15:15:17 +0800

    添加了鼠标移入移除事件，滑动切换图片

--- Swiper.vue
+++ Swiper.vue
@@ -1,12 +1,14 @@
 <template>
 	<!-- 轮播 -->
-	<div id="banner" >
-		<div class="container" 
+	<div id="banner" @mouseover="mouseover()" @mouseout="mouseout()">
+		<div class="container" ref="container"
 		:style="{transition:slideTime+'s',marginLeft:-screen*num+'px'}">
 			<!-- 轮播图片所在容器 -->
 				<img v-for="(item,index) in bannerImg" 
 				:src="item.img" 
-				:key="index">
+				:key="index" 
+				@touchstart="touchstart" 
+				@touchmove="touchmove" @touchend="touchend">
 			<!-- 指示点 -->
 		</div>
 			<div class="buttons">
@@ -47,11 +49,14 @@
 				timer:null,//定时器
 				screen:null,//屏幕宽度
 				reTime:3000 ,//图片停留时间
-				currentIndex:null//图片的下标
+				currentIndex:null,//图片的下标
+				startPointX:0,//移动端:初始触摸位置
+				changePointX:0,//移动端;改变的位置
+				showIndex:0//展示图片的index
 			}
 		},
 		methods:{
-			// 判断当前图片的index是否和图片个数相等,相等返回true,绑定的动态样式生效
+			// 判断当前图片的index是否和图片数相等,相等返回true,绑定的动态样式生效
 			currentBtn(index){
 				if(index == this.num){
 					return true
@@ -66,8 +71,7 @@
 			},
 			// 图片轮播
 			get(){
-				// 获取屏幕宽度值给screen
-				this.screen = document.body.clientWidth;
+				
 				// 当图片滑动=3时,将滑动时间置0,将图片数置为0,滑动时间设置为0视觉不会出现倒退
 				if(this.num == this.bannerImg.length-1){
 					this.slideTime = 0
@@ -77,11 +81,58 @@
 					this.slideTime = 1
 					this.num++
 				}
+			},
+			// 鼠标移入移除事件,移动轮播后清除轮播效果，展示的当前图片，移出后添加
+			mouseover(){
+				clearInterval(this.timer)
+			},
+			mouseout(){
+				this.timer = setInterval(this.get,this.reTime)
+			},
+			// 滑动展示轮播
+			slideShow(index){
+				let slide = this.$refs.container
+				slide.style.marginLeft=`-${this.screen*index}px`;//设置样式距离左侧***像素
+				console.log(this.screen)
+				console.log(index)
+			},
+			// 移动端:触摸开始位置
+			touchstart(e){
+				this.startPointX = e.changedTouches[0].pageX;
+				//触摸时暂时清除轮播效果
+				clearInterval(this.timer)
+				// console.log(this.startPointX)
+			},
+			//移动位置
+			touchmove(e){
+				let currentPointX = e.changedTouches[0].pageX;//当前x数值
+				if(this.startPointX-currentPointX>50 && this.showIndex<this.bannerImg.length-1){
+					clearInterval(this.timer)
+					setInterval(this.slideShow(++this.showIndex),1)
+					
+					console.log('向左滑动>50')
+					console.log(e)
+					
+				}else if(this.startPointX-currentPointX<-50 && this.showIndex>0){
+					console.log('向右滑动>50')
+					this.slideShow(--this.showIndex)
+				}
+
+			},
+			// 触摸结束添加轮播效果
+			touchend(){
+				this.timer = setInterval(this.get,this.reTime)
 			}
 		},
 		// 页面创建完成后执行该函数
 		mounted(){
 			this.timer = setInterval(this.get,this.reTime)
+			// 获取屏幕宽度值给screen
+			this.screen = document.body.clientWidth;
+		},
+		// 页面销毁执行该函数
+		beforeDestroy () {
+			clearInterval(this.timer)
 		}
 	}
 </script>
